20d633383b00a7897547365ca251d99ad3648eca
adarsh <adarshshrinet@outlook.com>
2022-01-13 18:53:27 +0530

    for static array

--- insertion.c
+++ insertion.c
@@ -1,15 +1,18 @@
 #include<stdio.h>
 void main()
 {
-    int arr[5];
+    int arr[7]={5,1,3,1,1,5,1};
+    int n=7;
+
     int i, v, j;
-    printf("Enter 5 elements\n");
-    for (i = 0; i < 5; i++)
-    {
-    printf("Enter element: ");    
-    scanf("%d",&arr[i]);
-    }
-    for(i=0; i<5;i++)
+    // printf("Enter 5 elements\n");
+    // for (i = 0; i < 5; i++)
+    // {
+    // printf("Enter element: ");    
+    // scanf("%d",&arr[i]);
+    // }
+
+    for(i=0; i<n;i++)
     {
         v=arr[i];
         j=i-1;
@@ -21,7 +24,7 @@ void main()
     }
     arr[j+1]=v;
     }
-    for (i = 0; i < 5; i++)
+    for (i = 0; i < n; i++)
     {
     printf("%d\t",arr[i]);
     }
